982dbe989d495fa58279ce02b5a467027ca7d0d3
val17102 <32184574+val17102@users.noreply.github.com>
2017-11-16 19:59:22 -0600

    Update PedidoExtendido.java

--- RED/src/Red/PedidoExtendido.java
+++ RED/src/Red/PedidoExtendido.java
@@ -21,36 +21,67 @@ public class PedidoExtendido implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+     /**
+    * numero de identificacion del pedido extendido
+    */
     private Long id;
-    //Atributos
+     /**
+    * codigo del pedido extendido
+    */
     public int Codigo;
+     /**
+    * cantidad de producto que se comprara en el pedido extendido
+    */
     public int Cantidad;
+     /**
+    * numero del producto que se compra en el pedido extendido
+    */
     public int Producto;
     
      public PedidoExtendido(){}
-    
+     /**
+    * Constructor de clase que se encarga de asignar los valores de Codigo, Cantidad y Producto.
+    *@param Codigo codigo del pedido extendido
+    *@param Cantidad cantidad de producto que se compra
+    *@para Producto numero del producto que se compra
+    */
     public PedidoExtendido(int Codigo, int Cantidad, int Producto){
         this.Codigo = Codigo;
         this.Cantidad = Cantidad;
         this.Producto = Producto;
     }
 
-
+    /**
+    * Get id
+    *@return devuelve el atributo id
+    */
     public Long getId() {
         return id;
     }
-
+    
+    /**
+    * Set id
+    *@param id valor que se le asignara a id
+    */
     public void setId(Long id) {
         this.id = id;
     }
-
+    
+    /**
+    * Metodo que obtiene el hashCode
+    *@return hash
+    */
     @Override
     public int hashCode() {
         int hash = 0;
         hash += (id != null ? id.hashCode() : 0);
         return hash;
     }
-
+     /**
+    * Metodo equals que compara los id de el pedidoextendido actual y otro
+    *@param object objeto tipo PedidoExtendido que se quiere comparar
+    *@return devuelve verdadero si son iguales los  id dos objetos, devuelve falso si no son iguales
+    */
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
@@ -63,7 +94,11 @@ public class PedidoExtendido implements Serializable {
         }
         return true;
     }
-
+    
+    /**
+    * Metodo que devuelve en forma de texto el valor de id
+    *@return devuelve el valor de id en forma de texto
+    */
     @Override
     public String toString() {
         return "Red.PedidoExtendido[ id=" + id + " ]";
